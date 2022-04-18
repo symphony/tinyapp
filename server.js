7be203d26f5a287599b5be63164aa850ed332cb2
symphony <jciurc@gmail.com>
2022-04-18 17:49:29 -0600

    hyperlinks working on urls page

--- server.js
+++ server.js
@@ -2,28 +2,34 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// config
 app.set('view engine', 'ejs');
 
+// our database
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// middleware
+// (none yet)
+
+// incoming requests
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
-app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get('/urls/:id', (req, res) => {
+  const templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render('urls_show', templateVars);
 });
 
-app.get('/hello', (req, res) => {
-  const templateVars = { greeting: 'hello u' };
-  res.render('hello_world', templateVars);
-});
-
+// Start server
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
