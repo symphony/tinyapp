796798dfd75f13e565c4f2031d9f8e170c97f56e
symphony <jciurc@gmail.com>
2022-04-18 17:06:57 -0600

    testing ejs templates

--- server.js
+++ server.js
@@ -2,22 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get('/hello', (req, res) => {
+  const templateVars = { greeting: 'hello u' };
+  res.render('hello_world', templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
