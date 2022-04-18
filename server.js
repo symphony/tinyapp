c1f46d83892cda5037b8fe7b14050f16b8400e0a
symphony <jciurc@gmail.com>
2022-04-18 17:19:09 -0600

    short url page renders properly

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render('urls_show', templateVars);
+});
+
 app.get('/hello', (req, res) => {
   const templateVars = { greeting: 'hello u' };
   res.render('hello_world', templateVars);
