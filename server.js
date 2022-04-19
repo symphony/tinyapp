bfab6049b26d59a11a2699812f3483db20c4a283
symphony <jciurc@gmail.com>
2022-04-18 19:50:55 -0600

    added working delete button

--- server.js
+++ server.js
@@ -13,8 +13,7 @@ const urlDatabase = {
 };
 
 // our functions
-const generateRandomString = () => {
-  const length = 6;
+const generateRandomString = (length = 6) => {
   const pool = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
   let newString = '';
@@ -60,10 +59,17 @@ app.get('/*', (req, res) => {
   res.redirect('/urls');
 });
 
+// Add new url
 app.post('/urls', (req, res) => {
   const newId = generateRandomString();
   urlDatabase[newId] = req.body.longURL;
-  res.redirect('/urls/' + newId);
+  res.redirect('/urls' + newId);
+});
+
+// Delete url
+app.post('/urls/:id/delete', (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect('/urls');
 });
 
 // Start server
